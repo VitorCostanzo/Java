9686dbaf3eef495b380bd3495c748e3ddaac38fd
VitorCostanzo <vcostanzo1995@gmail.com>
2022-07-13 18:29:54 -0300

    removendo lixo

--- Caderno/Sobrecrga/src/Sobrecarga.java
+++
@@ -1,7 +0,0 @@
-public class Sobrecarga {
-    public static void main(String[] args) {
-        User user1 = new User("Vitor", "Costanzo");
-        System.out.println(user1.fullName());
-        System.out.println(user1.fullName(false));
-    }
-}
