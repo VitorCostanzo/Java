cdc34f7c89da660cdbad551e393cc0f790084e76
VitorCostanzo <vcostanzo1995@gmail.com>
2022-07-13 21:19:44 -0300

    java.io

---
+++ CursoJava/src/br/com/cursojava/mainlibs/io/LerEscreverArquivo.java
@@ -0,0 +1,46 @@
+package br.com.cursojava.mainlibs.io;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+
+public class LerEscreverArquivo {
+
+	public static void main(String[] args) throws Exception {
+
+		// InputStreams -> recebem dados binarios (bits, bytes)
+		// Readers -> leem as entradas input e transformam em caracteres/strings/imagens
+
+		// Fluxo de entrada com Arquivo // -> Padrão Decorator
+		FileInputStream fis = new FileInputStream("Arquivo.txt"); // Recebe o binario do arquivo
+		InputStreamReader isr = new InputStreamReader(fis); // lê o binario e transforma em caracteres
+		BufferedReader br = new BufferedReader(isr); // Buffer para guardar os caracteres em linha
+
+		ArrayList<String> arq = new ArrayList<String>();
+		String linha = br.readLine();
+		while (linha != null) {
+			arq.add(linha);
+			linha = br.readLine();
+		}
+		br.close(); // Fechar arquivo SEMPRE
+
+		
+		// Fluxo de saida // -> Padrão Decorator
+		FileOutputStream fos = new FileOutputStream("Arquivo.txt");
+		OutputStreamWriter osw = new OutputStreamWriter(fos);
+		BufferedWriter bw = new BufferedWriter(osw);
+		
+		for (String row : arq) {
+			bw.write(row);
+			bw.newLine();
+		}
+		
+		bw.close();
+		
+	}
+
+}
---
+++ CursoJava/src/br/com/cursojava/mainlibs/io/LerTecladoEscreverArquivo.java
@@ -0,0 +1,47 @@
+package br.com.cursojava.mainlibs.io;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+
+public class LerTecladoEscreverArquivo{
+
+	public static void main(String[] args) throws Exception{
+		
+		InputStream is = System.in;
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+		
+		OutputStream os1 = System.out;
+		OutputStreamWriter osw1 = new OutputStreamWriter(os1);
+		BufferedWriter bw1 = new BufferedWriter(osw1);
+		
+		
+		ArrayList<String> arq = new ArrayList<String>();
+		String linha = br.readLine();
+		while (linha != null && !linha.isEmpty()) {
+			arq.add(linha + "\n");
+			bw1.write(linha);
+			bw1.flush(); // limpa o buffer // -> irá escrever imediatamente no console
+			linha = br.readLine();
+		}
+		br.close();
+		
+		
+		OutputStream os2 = new FileOutputStream("LerTecEscArq.txt");
+		OutputStreamWriter osw2 = new OutputStreamWriter(os2);
+		BufferedWriter bw2 = new BufferedWriter(osw2);
+		
+		for (String row : arq) {
+			bw2.write(row);
+		}
+		
+		bw2.close();
+	} 
+	
+}
